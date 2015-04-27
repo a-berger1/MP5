eb83ef7a21b08c0b1973912d40e65f5203374178
andy <andy@140.228.117.198>
2015-04-27 15:05:41 -0400

    End of Sunday

--- src/OrGate.java
+++ src/OrGate.java
@@ -1,9 +1,11 @@
+
+import java.awt.Graphics;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Noah
@@ -25,6 +27,19 @@ public class OrGate extends Gate {
             return true;
         }
         return false;
+
+    }
+
+    @Override
+    public void paint(Graphics g1) {
+        super.paint(g1);
+        g1.drawLine(30, 0, 30, 25);
+        g1.drawLine(70, 0, 70, 25);
+        
+        g1.drawArc(5, -37, 90, 70, 220, 100);
+        g1.drawLine(15, 60, 15, 20);
+        g1.drawLine(85, 60, 85, 20);
+        g1.drawArc(15, 25, 70, 70, 180, 180);
 
     }
 
